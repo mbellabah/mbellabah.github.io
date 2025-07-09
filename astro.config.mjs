6241d173e2cb247f9a6ddb7daf8290502371d7c8
Bella Bah <mobellabah@gmail.com>
2025-07-09 15:00:01 -0700

    chore: convert all the posts to the .mdx format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import remarkGfm from 'remark-gfm';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.bellabah.com',
-  integrations: [tailwind()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath, remarkGfm],
+      rehypePlugins: [[rehypeKatex, { strict: false, throwOnError: false }]]
+    }),
+    tailwind()
+  ],
   markdown: {
-    remarkPlugins: [
-      'remark-math',
-      'remark-gfm',
-      'remark-footnotes'
-    ],
-    rehypePlugins: [
-      ['rehype-katex', { 
-        strict: false,
-        throwOnError: false
-      }]
-    ]
+    remarkPlugins: [remarkMath, remarkGfm],
+    rehypePlugins: [[rehypeKatex, { strict: false, throwOnError: false }]]
   }
 });
